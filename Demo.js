37cd5524b719ca03d8a2bd237d305e4c4c74b403
Mika Rautio <1129758+mrautio@users.noreply.github.com>
2025-08-31 14:05:31 +0000

    cleanup

--- Demo.js
+++ Demo.js
@@ -11,24 +11,6 @@ window.globalTime = 0.0;
 
 includeFile('multiSceneEffects/PostProcess.js');
 includeFile('multiSceneEffects/dof.js');
-//includeFile('multiSceneEffects/EffectExplosion.js');
-//includeFile('multiSceneEffects/particleStream.js');
-//includeFile('multiSceneEffects/EffectStarfield.js');
-//includeFile('multiSceneEffects/trailerText.js');
-
-//includeFile('sceneIntro/intro.js');
-//includeFile('sceneZeus/zeus.js');
-//includeFile('sceneAgod/agod.js');
-//includeFile('sceneOuroboros/ouroboros.js');
-//includeFile('sceneCatBattle/catBattle.js');
-//includeFile('sceneLargeBattle/largeBattle.js');
-//includeFile('sceneMap/map.js');
-//includeFile('sceneCar/car.js');
-//includeFile('sceneChoice/choice.js');
-//includeFile('sceneBossFight/bossfight.js');
-//includeFile('sceneSuvanto/suvanto.js');
-//includeFile('sceneDestruction/destruction.js');
-//includeFile('sceneOutro/outro.js');
 
 includeFile('sceneIntro/intro.js');
 includeFile('sceneAbstract/abstract.js');
@@ -95,23 +77,6 @@ Demo.prototype.cameraSetup = function (stopCamAt) {
         }
   });       
 
-  /*this.loader.addAnimation({
-      "light": {
-          "type": "Directional",
-          "properties": { "intensity": 1.0 },
-          "castShadow": false,
-          "shadowProperties": {
-            "bias": ()=>Sync.get('Cam:ShadowBias'),
-            "radius": ()=>Sync.get('Cam:ShadowRadius'),
-            "normalBias": ()=>Sync.get('Cam:ShadowNormalBias')
-          } 
-      }
-      ,position:[{x:()=>window.camPos[0],y:()=>window.camPos[1],z:()=>window.camPos[2]+2.0}]
-      
-      ,"color": [{
-          "r": ()=>Sync.get('Light:R'), "g": ()=>Sync.get('Light:G'), "b": ()=>Sync.get('Light:B')
-      }]
-  });   */
 
 };
 
@@ -145,41 +110,12 @@ settings.demo.fbo.color.texture.magFilter = 'NearestFilter';
 //settings.demo.fbo.color.texture.wrapT = 'RepeatWrapping';
 
 Demo.prototype.init = function () {
-  const start = 0;
-  const duration = 380;
   const bpm = 135;
   const beat = 60/bpm;
-  const pattern = beat*8;
 
-  //this.loader.addAnimation({image:'_embedded/defaultWhite.png'});
-  //return;
   this.sceneAbstract();
   this.sceneIntro();
   this.sceneOverlay();
-
-  //this.addSongTitle();
-  //this.addSmileys();
-
-  /*this.loader.addAnimation({
-    image: '_embedded/defaultTransparent.png',
-    shader:{name:'vignette.fs'}
-  });*/
-
-  //return;
-
-  /*this.sceneIntro();
-  this.sceneAgod();
-  this.sceneZeus();
-  this.sceneCatBattle();
-  this.sceneMap();
-  this.sceneCar();
-  this.sceneChoice();
-  this.sceneBossFight();
-  this.sceneSuvanto();
-  this.sceneDestruction();
-  this.sceneOuroboros();
-  this.sceneLargeBattle();
-  this.sceneOutro();*/
 
   this.loader.setScene('main');
 
@@ -188,36 +124,6 @@ Demo.prototype.init = function () {
     {start: 0*window.pattern, duration: 5*window.pattern, name: 'intro', dof:false, polaroid:false},
     {start: 52.0*window.pattern, duration: 4*window.pattern, name: 'intro', dof:false, polaroid:false},
     {start: 4*window.pattern, duration: 51*window.pattern, name: 'overlay', dof:false, polaroid:false},
-    //{start: 0*window.pattern, duration: 9.25*window.pattern, name: 'intro', dof:true, polaroid:false},
-    //{start: 9.25*window.pattern, duration: 4*window.pattern, name: 'angered', dof:true, polaroid:false},
-    //{start: 13.25*window.pattern, duration: 2*window.pattern, name: 'agod', dof:true, polaroid:false},
-    //{start: 15.25*window.pattern, duration: 2*window.pattern, name: 'zeus', dof:true, polaroid:false},
-    //{start: 17.25*window.pattern, duration: 2*window.pattern, name: 'ouroboros', dof:true, polaroid:false},
-    //{start: 19.25*window.pattern, duration: 1*window.pattern, name: 'catBattle', dof:true, polaroid:false},
-    //{start: 20.25*window.pattern, duration: 1*window.pattern, name: 'map', dof:true, polaroid:false}, 
-    //{start: 21.25*window.pattern, duration: 1*window.pattern, name: 'car', dof:true, polaroid:false},
-    //{start: 22.25*window.pattern, duration: 1*window.pattern, name: 'choice', dof:true, polaroid:false},
-    //{start: 23.25*window.pattern, duration: 1*window.pattern, name: 'bossFight', dof:true, polaroid:false},
-    //{start: 24.25*window.pattern, duration: 1*window.pattern, name: 'largeBattle', dof:true, polaroid:false},
-    //{start: 25.25*window.pattern, duration: 2*window.pattern, name: 'suvanto', dof:true, polaroid:false},
-    //{start: 27.25*window.pattern, duration: 4*window.pattern, name: 'destruction', dof:true, polaroid:false},
-    //{start: 31.25*window.pattern+window.beat, duration: 16, name: 'outro', dof:false, polaroid:false},
-
-    /*Cat & ak scene:
-Non-stop action
-
-Map scene:
-Make strategical choices
-
-Car chase:
-1000% adrenalin
-
-Choice scene (Kill people / save people):
-Meaningful choices
-
-All seeing eye + army:
-Epic battles*/
-
   ];
 
   scenes.forEach((scene) => {
